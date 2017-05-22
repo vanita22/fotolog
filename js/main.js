e52fd9b18d00f067de8c4e35a204534a72989c41
vanita22 <vanitayrafita2004@gmail.com>
2017-05-22 01:53:32 -0300

    subiendo archivos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 function agregaPosteo() {
 //escribe aca el codigo
+var containerPosteos = document.getElementById("contenedor-posteos");
+var nombreFF = document.getElementById("nombre").value;
+var contenidoFF = document.getElementById("cajaposteos").value;
+
+
+var nuevoPost = document.createElement("div");
+var contenedorNombre = document.createElement("strong");
+var contenedorPost = document.createElement("p");
+var separacion = document.createElement("hr");
+var parrafCorazon = document.createElement("p");
+var i = document.createElement("i");
+
+parrafCorazon.appendChild(i);
+
+parrafCorazon.setAttribute("class", "corazon");
+	i.setAttribute("class", "fa fa-heart");
+	i.setAttribute("arial-hidden", "true");
+
+var nodoNombre = document.createTextNode(nombreFF + "escribió: ");
+var nodoPosteo = document.createTextNode(contenidoFF);
+
+contenedorNombre.appendChild(nodoNombre);
+contenedorPost.appendChild(nodoPosteo);
+
+nuevoPost.appendChild(contenedorNombre);
+nuevoPost.appendChild(contenedorPost);
+nuevoPost.appendChild(parrafCorazon);
+nuevoPost.appendChild(separacion);
+
+i.addEventListener("click", function(){
+	i.classList.toggle("rojo");
+});
+
+nuevoPost.setAttribute("class", "posteo");
+
+containerPosteos.appendChild(nuevoPost);
+
+document.getElementById("nombre").value = "";
+document.getElementById("cajaposteos").value = "";
 }
